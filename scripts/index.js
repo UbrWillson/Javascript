32e78fb6e4903c3e13564feac88ff2189eada8c9
Vlad Savenko <114161862+UbrWillson@users.noreply.github.com>
2023-04-06 18:58:49 +0300

    finished lesson

--- scripts/index.js
+++ scripts/index.js
@@ -1,1 +1,103 @@
+// use strict строгий режим
 
+// // var name = "Alex"
+// let name_1 = "Dmytro";
+// // const name_3 = "Lizzy";
+
+// console.log(name_1);
+
+// const DEFAULT_URL_DS = "http/localhost"
+// const ROLE = "ADMIN"
+
+// let isActivated = true;
+// isActivated = false;
+
+// let number = 16; //number
+// // let bigInt = 9000000000000000000000000000000000000000; //bigint
+// let string = "Alex";
+// string = "Alex"
+// // String = "Alex ${number}"
+// string = "Alex" + number;
+
+// string = `9000`
+// console.log(string)
+// let js = Symbol("js")//symbol
+// let name;
+// console.log(name)//Не визначений елемент
+
+// let password = null; //null-Взагалі нічого немає
+
+// let isAdmin = true; // or false (0 or 1)
+
+// let obj = {
+//     name: "Dmytro",
+//     age: 53
+// };
+
+// console.log("Some Text")
+// console.error("Some Text")
+// console.info("Some Text")
+// console.table("Some Text")
+// console.warn("Some Text")
+
+// alert("Привіт")
+// confirm("Ти Адмін?")
+// let age = prompt("Який у тебе вік?", 18);
+// console.log(prompt)
+// console.log(`Ваш вік -${age}`)
+
+// let str = "Привіт + Владислав!";
+// console.log(str);
+
+// operators + - / * %
+
+// let a = 4;
+// let b = 10;
+
+// console.log(a + b)
+
+// ++ -- postfixed operators
+
+// let num = 10;
+
+// console.log(num++)
+// console.log(num)
+
+// ++ -- prefixed operators
+
+// let num = 10;
+
+// console.log(++num)
+// console.log(num)
+
+// let age = prompt("Який у вас вік")
+
+// if (age < 18) {
+//     console.log("Замалий")
+// }
+
+// if(age > 18 ) {
+//     console.log("Проходиш")
+// }
+
+// let role = prompt("Яка у вас роль?");
+
+// if(role == "admin") {
+//  console.log("Ви адмін")};
+// if(role !== "admin") {
+// console.log("Ви користувач")};
+
+// let role = prompt("Хто ви?");
+
+// switch (role) {
+//   case "admin":
+//     console.log("admin");
+//     break;
+
+//   case "user":
+//     console.log("user");
+//     break;
+
+//   default:
+//     console.log("Нема такого");
+// }
