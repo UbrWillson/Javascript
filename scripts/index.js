47e0da0a8e0c570e4c75e4a829f521ed98b0e6fb
Vlad Savenko <114161862+UbrWillson@users.noreply.github.com>
2023-04-11 22:23:48 +0300

    123

--- scripts/index.js
+++ scripts/index.js
@@ -1,1 +1,92 @@
 
+//math
+
+// console.log(Math.round(x: 5.7)) //6
+// console.log(Math.round( x: 5.2)) //5
+
+
+// console.log(Math.floor( x: 5.7)) //5
+// console.log(Math.ceil( x: 5.2)) //6
+
+// console.log(Math.pow(3, 4)) //81
+
+// console.log(Math.random()) // random
+
+
+// const random = (min, max) => {
+//     return Math.random()
+// }
+// console.log(random(100, 1000))
+
+
+// const random = (min,max) => {
+//     return Math.floor( Math.random() * (max - min) + min)
+
+// }
+
+// console.log(random(4,12))
+
+
+
+
+
+//cycles
+
+// const arr = [1, 5, 6, 7, 8, 4, 3,];
+// let i = 0
+
+// while(true){
+//     console.log(arr)
+//     console.log(arr[i])
+//     i++
+    
+// }
+
+// do {
+//     console.log(arr[i])
+//     i++
+// }
+// for(let i = 0; i < arr.length; i++){
+
+// }
+
+// const people = [
+//     {name: "ALEX", age: 25},
+//     {name: "VLAD", age: 21},
+//     {name: "DANYA", age: 25},
+//     {name: "DAN", age: 35},
+//     {name: "VANYA", age: 26},
+// ]
+
+// for(let i = 0; i< people.length; i++) {
+//     console.log(`${people[i].name} ${people[i].age}`)
+// }
+
+// const arr = [1, 4, 234, 23, 34, 324]
+// let i = 0
+
+// let letters = function(){
+
+//     let summ = 0;
+//     for(let i = 0; i < arr.length; i++) {
+//         summ += arr [i]
+//     }
+//     return summ
+// }
+// document.write(letters())
+
+
+
+// const names = ["max", "alex", "vlad",]
+// // додає значення
+// // names.push("dan")
+
+// // // забирає значення
+// // const name = names.pop();
+// // забирає перше значення
+// // names.shift(names);
+// // додає перше значення
+// // names.unshift("alex")
+
+
+// console.log(names)
