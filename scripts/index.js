1ba030ba039569c59a5eff7a7837d11cc997f8fc
Vlad Savenko <114161862+UbrWillson@users.noreply.github.com>
2023-04-13 20:21:43 +0300

    43432

--- scripts/index.js
+++ scripts/index.js
@@ -1,1 +1,9 @@
+const arr_numbers = ['14', '16', '20', '12', '18']
 
+function show_array(){
+    let avarage_number = (14+16+20+12+19)/5;
+    if(arr_numbers > avarage_number){
+        arr_numbers.sort()
+    }
+}
+alert(show_array())
