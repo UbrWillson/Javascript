49d0bd2a506c0752e2d04375bb75d4ac8fbfa906
Vlad Savenko <114161862+UbrWillson@users.noreply.github.com>
2023-04-13 22:16:28 +0300

    56434534

--- scripts/index.js
+++ scripts/index.js
@@ -1,4 +1,4 @@
-const arr_numbers = ["14", "16", "20", "12", "18"];
+const arr_numbers = [14, 16, 20, 12, 18, 20, 30];
 
 // function show_array() {
 //   let avarage_number = (14 + 16 + 20 + 12 + 19) / 5;
@@ -8,12 +8,22 @@ const arr_numbers = ["14", "16", "20", "12", "18"];
 // }
 // alert(show_array());
 
+// function show_array(array_numbers) {
+//   let avarage_number = 0;
+//   for (let i = 0; i < array_numbers.length; i++) {
+//     avarage_number += array_numbers[i];
+//   }
+
+//   avarage_number /= array_numbers.length;
+//   let new_arr = [];
+//   for (let i = 0; i < array_numbers.length; i++) {
+//     if (array_numbers[i] > avarage_number) {
+//       new_arr.push(array_numbers[i]);
+//     }
+//   }
+//   return new_arr;
+// }
+// console.log(show_array(arr_numbers))
 
 
 
-function show_array() {
-
-    let avarage_number = (14 + 16 + 20 + 12 + 19) / 5;
-for(let i = 0; i < )
-
-}
