1e752d568cb428ee73d194c77d98bbb3fe96c293
Vlad Savenko <114161862+UbrWillson@users.noreply.github.com>
2023-04-13 20:36:06 +0300

    353453

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,19 @@
-const arr_numbers = ['14', '16', '20', '12', '18']
+const arr_numbers = ["14", "16", "20", "12", "18"];
 
-function show_array(){
-    let avarage_number = (14+16+20+12+19)/5;
-    if(arr_numbers > avarage_number){
-        arr_numbers.sort()
-    }
+// function show_array() {
+//   let avarage_number = (14 + 16 + 20 + 12 + 19) / 5;
+//   if (arr_numbers > avarage_number) {
+//     arr_numbers.sort();
+//   }
+// }
+// alert(show_array());
+
+
+
+
+function show_array() {
+
+    let avarage_number = (14 + 16 + 20 + 12 + 19) / 5;
+for(let i = 0; i < )
+
 }
-alert(show_array())
