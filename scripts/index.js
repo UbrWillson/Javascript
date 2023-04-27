fd266a029d10c4c070248b0459e7b17325e8a8a7
Vlad Savenko <114161862+UbrWillson@users.noreply.github.com>
2023-04-27 22:35:06 +0300

    not finished yet

--- scripts/index.js
+++ scripts/index.js
@@ -1,1 +1,17 @@
 
+const phone_arr = [
+    {id:1,name:"iphone",memory:"64gb"},
+    {id:2, name:"samsung",memory:"128gb"},
+    {id:3, name:"Mototorola", memory:"32gb"},
+    {id:4, name:"Xiaomi", memory:"512gb"}
+]
+let question = prompt("напишіть назву телефона")
+let memory = prompt("Напишіть об'єм пам'яті")
+let added_phone= {id: phone_arr.length+1, name: question, memory: memory}
+let date = new Date();
+function add_phone(){
+    phone_arr.push(added_phone)
+    return phone_arr;
+}
+console.log(add_phone())
+document.write(date)
