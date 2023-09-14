81d73d3fe30c2f51af4b8a4b183ecc524443e0de
Vlad Savenko <114161862+UbrWillson@users.noreply.github.com>
2023-09-14 21:17:57 +0300

    must be finished

--- scripts/index.js
+++ scripts/index.js
@@ -1,4 +1,20 @@
+let lb_sound = document.querySelector('#lb_sound'); 
+    let btn = document.querySelector('.btn');
+    let Name = document.querySelector('#name');
+    let gravity = document.querySelector('#gravity');
+    let climate = document.querySelector('#climate');
+    let terrain = document.querySelector('#terrain');
 
+document.addEventListener('DOMContentLoaded', function() {
+    
+    btn.addEventListener('click', function() { 
+        lb_sound.play();
+    });
+});
 
+function getData(){
+    let randomPlanets = Math.floor((Math.random() *61) +1)
+    let swapi = 'https://swapi.dev/api/planets' + randomPlanets;
+}
 
 
