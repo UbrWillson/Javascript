464dcd55f54e95da64f51bd6976ee5ec67e895fa
Vlad Savenko <114161862+UbrWillson@users.noreply.github.com>
2023-09-26 17:47:10 +0300

    finished

--- scripts/index.js
+++ scripts/index.js
@@ -10,11 +10,44 @@ document.addEventListener('DOMContentLoaded', function() {
     btn.addEventListener('click', function() { 
         lb_sound.play();
     });
+
+    function getData() {
+        generateDataLoading();
+        let randomPlanet = Math.floor(Math.random() * 61) + 1;
+        let swApi = `https://swapi.dev/api/planets/${randomPlanet}/`;
+    
+        fetch(swApi)
+            .then(response => response.json())
+            .then(data => generateData(data))
+            .catch(e => generateDataFail());
+    }
+    
+    function generateData(data) {
+        Name.innerText = "Name of the planet: " + data.name;
+        gravity.innerText = "Gravity of the planet: " + data.gravity;
+        climate.innerText = "Climate of the planet: " + data.climate;
+        terrain.innerText = "Terrain of the planet: " + data.terrain;
+    }
+    
+    function generateDataFail() {
+        Name.innerText = 'not working'
+        gravity.innerText = ''
+        climate.innerText = ''
+        terrain.innerText = ''
+    }
+    
+    function generateDataLoading() {
+        Name.innerHTML = ''
+        gravity.innerText = ''
+        climate.innerText = ''
+        terrain.innerText = ''
+    }
+    
+    btn.addEventListener('click', getData);
+
 });
 
-function getData(){
-    let randomPlanets = Math.floor((Math.random() *61) +1)
-    let swapi = 'https://swapi.dev/api/planets' + randomPlanets;
-}
+
+
 
 
