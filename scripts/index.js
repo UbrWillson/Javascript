e69a1c4bf6e3621047d623ddf9f972a901f709a2
Vlad Savenko <114161862+UbrWillson@users.noreply.github.com>
2023-10-03 03:13:13 +0300

    finished

--- scripts/index.js
+++ scripts/index.js
@@ -1,1 +1,19 @@
+let doc = document;
+let btn = doc.querySelector(".btn");
+let img = doc.querySelector("#img");
+let inpt = doc.querySelector("#text_inpt");
 
+btn.onclick = function get_dog() {
+    let url = `https://dog.ceo/api/breed/${inpt.value}/images/random`;
+
+    console.log(url);
+
+    fetch(url)
+        .then(response => response.json())
+        .then(data => {
+            img.src = data.message;
+        })
+        .catch(error => {
+            console.error("Помилка при отриманні зображення собаки:", error);
+        });
+};
