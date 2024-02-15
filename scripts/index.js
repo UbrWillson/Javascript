084087ba00aa9ca8b9217b3c1ed385878503f5a5
Vlad Savenko <114161862+UbrWillson@users.noreply.github.com>
2024-02-15 04:46:07 +0200

    builded

--- scripts/index.js
+++ scripts/index.js
@@ -1,19 +1,33 @@
 let doc = document;
 let btn = doc.querySelector(".btn");
+let btnB = doc.querySelector(".btn-breed")
 let img = doc.querySelector("#img");
 let inpt = doc.querySelector("#text_inpt");
+let input = doc.querySelector("#text_input");
 
 btn.onclick = function get_dog() {
-    let url = `https://dog.ceo/api/breed/${inpt.value}/images/random`;
-
-    console.log(url);
+    let url = `https://dog.ceo/api/breed/${inpt.value}/images/random`; 
 
     fetch(url)
         .then(response => response.json())
         .then(data => {
-            img.src = data.message;
+            img.src = data.message; 
         })
         .catch(error => {
             console.error("Помилка при отриманні зображення собаки:", error);
         });
 };
+
+btnB.onclick = function dog_list() {
+    let url = `https://dog.ceo/api/breed/hound/${input.value}/images`;
+
+    fetch(url)
+        .then(response => response.json())
+        .then(data => {
+            let randomIndex = Math.floor(Math.random() * data.message.length); 
+            img.src = data.message[randomIndex]; 
+        })
+        .catch(error => {
+            console.error("Помилка при отриманні зображення собаки:", error);
+        });
+}
